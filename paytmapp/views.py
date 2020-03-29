2a0a8f459f7e1833c0a9759a85bb471d3d453e19
bhatiaisb5300 <52737993+bhatiaisb5300@users.noreply.github.com>
2020-03-29 21:50:46 +0530

    initial

--- paytmapp/views.py
+++ paytmapp/views.py
@@ -23,7 +23,7 @@ def index(request):
             'INDUSTRY_TYPE_ID':'Retail',
             'WEBSITE':'DEFAULT',
             'CHANNEL_ID':'WEB',
-            'CALLBACK_URL':'http://http://159.89.170.49:7000/status/',
+            'CALLBACK_URL':'http://159.89.170.49:7000/status/',
         }
         data_dct['CHECKSUMHASH'] = generate_checksum(data_dct,MERCHANT_KEY)
         return render(request,'redirect.html',{'datas':data_dct})
