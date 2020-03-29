ccc9be8e3609caf2285df6a41d875c9585311998
bhatiaisb5300 <52737993+bhatiaisb5300@users.noreply.github.com>
2020-03-29 21:29:16 +0530

    initial

--- paytmapp/views.py
+++ paytmapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,render_to_response
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.template import RequestContext
